49df360f4fde854345f31ad048dedb5107b62059
Arthur Van Roost <arthur.vanroost@icloud.com>
2025-06-02 15:06:00 +0200

    crea pages component

---
+++ src/pages/content/Content.jsx
@@ -0,0 +1,12 @@
+import Footer from '../../components/footer/Footer'
+import Nav from '../../components/nav/Nav'
+import './content.css'
+
+export default function Content() {
+    return(
+        <>
+           <Nav/>
+           <Footer/>
+        </>
+    )
+}
